83fa8b4adbdb8426d5451ea17ca183e1e985bea5
obemyshinnyrod <miswah.md@gmail.com>
2022-04-23 21:26:42 +0530

    (logger): add sentry setup

--- server/index.js
+++ server/index.js
@@ -11,6 +11,10 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
 
+/**Sentry Import */
+const Sentry = require("@sentry/node");
+const Tracing = require("@sentry/tracing");
+
 /**Mongooes Conection Setup */
 mongoose
   .connect(process.env.MONGO_URI, { dbName: "InterviewLog", useNewUrlParser: true, useUnifiedTopology: true })
@@ -22,11 +26,41 @@ mongoose
     console.log("Mongo Connection Error", err);
   });
 
+/**Sentry Conection Setup */
+Sentry.init({
+  dsn: "https://0bcdb1c7dc5240fda085a31dead9704b@o1216628.ingest.sentry.io/6358651",
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app }),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
 /**Utilities Setup */
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+
+/**Sentry Handlers */
+// RequestHandler creates a separate execution context using domains, so that every
+// transaction/span/breadcrumb is attached to its own Hub instance
+app.use(
+  Sentry.Handlers.requestHandler({
+    serverName: true,
+    user: true,
+    ip: true,
+    transaction: "path",
+  })
+);
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
 
 /**Ping API Call to check for server health */
 app.get("/ping", (req, res) => {
@@ -35,6 +69,33 @@ app.get("/ping", (req, res) => {
     error: false,
     message: "Server is healthy",
   });
+});
+/**API call to debug sentry */
+app.get("/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My first Sentry error!");
+});
+
+/**Sentry Error handler */
+// The error handler must be before any other error middleware and after all controllers
+app.use(
+  Sentry.Handlers.errorHandler({
+    shouldHandleError(error) {
+      // Capture all 404 and 500 errors
+      //   if (error.status === 404 || error.status === 500) {
+      //     return true;
+      //   }
+      //   return false;
+      return true;
+    },
+  })
+);
+
+// Optional fallthrough error handler
+app.use(function onError(err, req, res, next) {
+  // The error id is attached to `res.sentry` to be returned
+  // and optionally displayed to the user for support.
+  res.statusCode = 500;
+  res.end(res.sentry + "\n");
 });
 
 /**Server Bootstraping */
