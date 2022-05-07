8e2027550e7806d17df7f703a4e28537b700a632
obemyshinnyrod <miswah.md@gmail.com>
2022-05-07 13:50:35 +0530

    (routing): add /auth for auth module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,17 @@
+/**Modules Immports */
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
+/**Component Imports */
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, RouterModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
