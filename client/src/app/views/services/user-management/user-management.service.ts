17c5915f00f884cc8021706eb0f2c2988ea1e6a9
obemyshinnyrod <miswah.md@gmail.com>
2022-05-21 20:33:45 +0530

    (feat): methods for login and signup add

--- client/src/app/views/services/user-management/user-management.service.ts
+++ client/src/app/views/services/user-management/user-management.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Signup, Login } from '../../shared/interfaces';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserManagementService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  /**
+   * It takes a body of type Signup and returns an Observable of type any
+   * @param {Signup} body - Signup
+   * @returns Observable&lt;any&gt;
+   */
+  singup(body: Signup): Observable<any> {
+    return this.http.post('/user/signup', body);
+  }
+
+  /**
+   * This function takes a Login object as a parameter and returns an Observable of any type.
+   * @param {Login} body - Login
+   * @returns Observable&lt;any&gt;
+   */
+  login(body: Login): Observable<any> {
+    return this.http.post('/user/login', body);
+  }
 }
