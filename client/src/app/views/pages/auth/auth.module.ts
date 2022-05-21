6cad78f682f34da8df8e5d8bb67444134178e1ca
obemyshinnyrod <miswah.md@gmail.com>
2022-05-21 20:34:15 +0530

    (enh): reactive forms add

--- client/src/app/views/pages/auth/auth.module.ts
+++ client/src/app/views/pages/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { AuthRoutingModule } from './auth-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AuthComponent } from './auth/auth.component';
 import { LoginComponent } from './login/login.component';
@@ -9,6 +10,6 @@ import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [AuthComponent, LoginComponent, SignupComponent],
-  imports: [CommonModule, AuthRoutingModule, RouterModule],
+  imports: [CommonModule, AuthRoutingModule, RouterModule, ReactiveFormsModule],
 })
 export class AuthModule {}
